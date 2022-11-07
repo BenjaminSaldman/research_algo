f7983e719f8a2633e4a2ffd9d4a6d5cda247980a
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-07 14:59:45 +0200

    updated to list

--- HW3/Ex3.py
+++ HW3/Ex3.py
@@ -1,10 +1,11 @@
 import doctest
 
 
-class List:
+class List(list):
 
-    def __init__(self, lst=list):
-        self._lst = lst
+    def __init__(self, *args):
+        super().__init__(*args)
+        # self._lst = lst
 
     def __getitem__(self, item):
         """
@@ -15,13 +16,16 @@ class List:
         >>> List([[[1, 2, 3, 33], [4, 5, 6, 66]],[[7, 8, 9, 99], [10, 11, 12, 122]],[[13, 14, 15, 155], [16, 17, 18, 188]],])[1, 1, 3]
         122
         """
-        ret = self._lst
         if isinstance(item, tuple):
-            for i in item:
+            ret = super().__getitem__(item[0])
+            for i in item[1:]:
                 ret = ret[i]
             return ret
         else:
-            return ret[item]
+            return super().__getitem__(item)
+
+    def __setitem__(self, key, value):
+        pass
 
 
 mylist = List([
@@ -30,6 +34,8 @@ mylist = List([
     [[13, 14, 15, 155], [16, 17, 18, 188]],
 ]
 )
+print(mylist)
+print(len(mylist))
 print(mylist[0, 1, 3])
 print(mylist[0])
 print(mylist[1, 1, 3])
