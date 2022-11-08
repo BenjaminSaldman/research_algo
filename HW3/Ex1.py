4111596c1fc26011a5307a963657bd3c8f8b38e8
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-08 15:26:30 +0200

    regex for Ex1

--- HW3/Ex1.py
+++ HW3/Ex1.py
@@ -1,6 +1,8 @@
 import re
 
-regex = r'\b[A-Za-z0-9+.-_{0,1}A-Za-z0-9+]+@[A-Za-z0-9+.-_{1,1}A-Za-z0-9+]+\.[A-Z|a-z]{2,}\b'
-
-print(re.fullmatch(regex, 'abc-@mail.com'))
+regex = r'\b[A-Za-z0-9+.%-_{0,1}A-Za-z0-9+]+@[A-Za-z0-9+.-_{1,1}A-Za-z0-9+]+\.[A-Z|a-z]{2,}\b'
+#regex = r'\b(A-Z|a-z|0-9)(A-Z*a-z*0-9*)[+.%-_](A-Z|a-z|0-9)(A-Z*a-z*0-9*)+@[A-Za-z0-9+.-_{1,1}A-Za-z0-9+]+\.[A-Z|a-z]{2,}\b'
+regex=r'\b([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]+)+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b'
+print(re.fullmatch(regex, 'abc%a.aaaa@mail.com'))
 print()
+
