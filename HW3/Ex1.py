3010da320195b19aaa4a45705955d271afacb72a
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-08 17:22:56 +0200

    end of ex1

--- HW3/Ex1.py
+++ HW3/Ex1.py
@@ -1,7 +1,23 @@
 import re
 
-regex = r'\b[A-Za-z0-9+.%-_{0,1}A-Za-z0-9+]+@[A-Za-z0-9+.-_{1,1}A-Za-z0-9+]+\.[A-Z|a-z]{2,}\b'
-# regex = r'\b(A-Z|a-z|0-9)(A-Z*a-z*0-9*)[+.%-_](A-Z|a-z|0-9)(A-Z*a-z*0-9*)+@[A-Za-z0-9+.-_{1,1}A-Za-z0-9+]+\.[A-Z|a-z]{2,}\b'
+# regex of valid email address according to: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
 regex = r'\b([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+@([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+\.[A-Za-z]{2,4}\b'
-print(re.fullmatch(regex, 'abc.def@mail.com'))
-print()
+filename = input("Please enter the file name: ")
+"""
+I assumed that each row contains 1 mail address.
+file.txt contains all the mail addresses that appearing in: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
+"""
+with open(filename, mode='r') as file:
+    text = file.readlines()
+valid = []
+invalid = []
+
+for i in text:
+    i = i.strip('\n\t\b')  # Ignore special characters.
+    if re.fullmatch(regex, i) is None:
+        invalid.append(i)
+    else:
+        valid.append(i)
+
+print(f"Valid: {valid}")
+print(f"Invalid: {invalid}")
