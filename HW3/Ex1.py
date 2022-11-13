0b6e190b75300f4a09b12d237c45619474b51aa7
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-13 20:38:59 +0200

    ex1 mail-end can have at least two unbounded

--- HW3/Ex1.py
+++ HW3/Ex1.py
@@ -2,6 +2,7 @@ import re
 
 # regex of valid email address according to: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
 regex = r'\b([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+@([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+\.[A-Za-z]{2,}\b'
+
 filename = input("Please enter the file name: ")
 """
 I assumed that each row contains 1 mail address.
