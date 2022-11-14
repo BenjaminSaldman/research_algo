55e81975e0dae1f5ecb16097c71d3eb0de303219
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-14 08:41:01 +0200

    added tests

--- HW3/Ex1.py
+++ HW3/Ex1.py
@@ -1,25 +1,37 @@
 import re
-
+import doctest
 # regex of valid email address according to: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
 # Helped with: https://www.codexpedia.com/regex/regex-symbol-list-and-regex-examples/
 regex = r'\b([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+@([A-Za-z0-9]+([A-Za-z0-9]*[._-][A-Za-z0-9])*[A-Za-z0-9]*)+\.[A-Za-z]{2,}\b'
 
-filename = input("Please enter the file name: ")
-"""
-I assumed that each row contains 1 mail address.
-file.txt contains all the mail addresses that appearing in: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
-"""
-with open(filename, mode='r') as file:
-    text = file.readlines()
-valid = []
-invalid = []
 
-for i in text:
-    i = i.strip('\n\t\b')  # Ignore special characters.
-    if re.fullmatch(regex, i) is None:
-        invalid.append(i)
-    else:
-        valid.append(i)
 
-print(f"Valid: {valid}")
-print(f"Invalid: {invalid}")
+
+
+def Ex1(filename: str):
+    """
+    I assumed that each row contains 1 mail address.
+    file.txt contains all the mail addresses that appearing in: https://help.xmatters.com/ondemand/trial/valid_email_format.htm
+    >>> Ex1("file.txt")
+    Valid: ['abc-d@mail.com', 'abc.def@mail.com', 'abc.def@mail.com', 'abc@mail.com', 'abc_def@mail.com', 'abc.def@mail.cc', 'abc.def@mail-archive.com', 'abc.def@mail.org', 'abc.def@mail.com']
+    Invalid: ['abc-@mail.com', 'abc..def@mail.com', '.abc@mail.com', 'abc.def@mail.c', 'abc.def@mail#archive.com', 'abc.def@mail', 'abc.def@mail..com', 'abc..def@mail.com', 'abc.def@mail..com']
+    """
+
+    with open(filename, mode='r') as file:
+        text = file.readlines()
+    valid = []
+    invalid = []
+
+    for i in text:
+        i = i.strip('\n\t\b')  # Ignore special characters.
+        if re.fullmatch(regex, i) is None:
+            invalid.append(i)
+        else:
+            valid.append(i)
+
+    print(f"Valid: {valid}")
+    print(f"Invalid: {invalid}")
+if __name__ == '__main__':
+    doctest.testmod()
+    filename = input("Please enter the file name: ")
+    Ex1(filename)
