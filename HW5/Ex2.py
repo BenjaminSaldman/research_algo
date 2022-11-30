4524f563b61b55203e0e77a793c06715148e9d98
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-30 18:03:37 +0200

    Fixed for presentation

--- HW5/Ex2.py
+++ HW5/Ex2.py
@@ -18,6 +18,18 @@ g = {
 }
 tsp = [[math.inf, 10, 15, 20], [10, math.inf, 35, 25], [15, 35, math.inf, 30], [20, 25, 30, math.inf]]
 
+tsp2 = [[math.inf, 10, 2, 5],
+        [10, math.inf, 3, 8],
+        [2, 3, math.inf, 2],
+        [5, 8, 2, math.inf]]
+
+g2 = {
+    'CITY1': [math.inf, ('CITY2', 1000), ('CITY3', 200), ('CITY4', 55789)],
+    'CITY2': [('CITY1', 10), math.inf, ('CITY3', 3), ('CITY4', 8)],
+    'CITY3': [('CITY1', 2), ('CITY2', 3), math.inf, ('CITY4', 2)],
+    'CITY4': [('CITY1', 5), ('CITY2', 8), ('CITY3', 2), math.inf]
+}
+
 
 class Output:
     """
@@ -39,7 +51,7 @@ class Output:
         return sum
 
 
-def construct_graph(graph, flag=False):
+def construct_graph(graph):
     """
     Graph constructor for both TSP algorithm.
     The greedy algorithm requires k*n graph and the 'brute force' requires n*n.
@@ -49,30 +61,23 @@ def construct_graph(graph, flag=False):
     if isinstance(graph, dict):
         for j, k in graph.items():
             for i in k:
-                if not flag:
-                    if i != math.inf:
-                        g.append((j, i))
-                else:
-                    g.append((j, i))
+                g.append((j, i))
             ret.append(g)
             g = []
     else:
         for i in range(len(graph)):
             for j in range(len(graph[i])):
-                if not flag:
-                    if graph[i][j] != math.inf:
-                        g.append((i, (j, graph[i][j])))
-                else:
-                    g.append((i, (j, graph[i][j])))
+                g.append((i, (j, graph[i][j])))
+
             ret.append(g)
             g = []
 
     return ret
 
 
-def TSP_PRINTER(algorithm: Callable, items: list, outputtype=Output.path, flag=False, **kwargs):
+def TSP_PRINTER(algorithm: Callable, items: list, outputtype=Output.path, **kwargs):
     """
-    The strategy design pattern, it gets an algorithm, graph, output algorithm and flag
+    The strategy design pattern, it gets an algorithm, graph, output algorithm and flag that is a
     and returns a path/sum of the hamiltonian cycle.
     >>> TSP_PRINTER(algorithm=greedy_tsp, items=g)
     'A -> B -> D -> C -> A'
@@ -82,16 +87,32 @@ def TSP_PRINTER(algorithm: Callable, items: list, outputtype=Output.path, flag=F
     80
     >>> TSP_PRINTER(algorithm=greedy_tsp, items=tsp, outputtype=Output.sum)
     80
-    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.sum, flag=True)
+    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.sum)
     80
-    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=tsp, outputtype=Output.sum, flag=True)
+    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=tsp, outputtype=Output.sum)
     80
-    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.path, flag=True)
+    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.path)
     'A -> B -> D -> C -> A'
-    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=tsp, outputtype=Output.path, flag=True)
+    >>> TSP_PRINTER(algorithm=brute_force_tsp, items=tsp, outputtype=Output.path)
     '0 -> 1 -> 3 -> 2 -> 0'
+    >>> TSP_PRINTER(algorithm=brute_force_tsp,items=tsp2,outputtype=Output.path)
+    '0 -> 2 -> 1 -> 3 -> 0'
+    >>> TSP_PRINTER(algorithm=brute_force_tsp,items=tsp2,outputtype=Output.sum)
+    18
+    >>> TSP_PRINTER(algorithm=greedy_tsp,items=tsp2,outputtype=Output.path)
+    '0 -> 2 -> 3 -> 1 -> 0'
+    >>> TSP_PRINTER(algorithm=greedy_tsp,items=tsp2,outputtype=Output.sum)
+    22
+    >>> TSP_PRINTER(algorithm=brute_force_tsp,items=g2,outputtype=Output.sum)
+    216
+    >>> TSP_PRINTER(algorithm=brute_force_tsp,items=g2,outputtype=Output.path)
+    'CITY1 -> CITY3 -> CITY2 -> CITY4 -> CITY1'
+    >>> TSP_PRINTER(algorithm=greedy_tsp,items=g2,outputtype=Output.sum)
+    220
+    >>> TSP_PRINTER(algorithm=greedy_tsp,items=g2,outputtype=Output.path)
+    'CITY1 -> CITY3 -> CITY4 -> CITY2 -> CITY1'
     """
-    path = algorithm(construct_graph(items, flag), **kwargs)
+    path = algorithm(construct_graph(items), **kwargs)
     return outputtype(path)
 
 
@@ -123,10 +144,11 @@ def greedy_tsp(graph):
             If we didn't visit this city (row) and the cost of the edge is minimal
             set it as a candidate.
             """
-            if not visited[next_row(graph, j[1][0])] and j[1][1] < min:
-                curr = row
-                next_city = next_row(graph, j[1][0])
-                min = j[1][1]
+            if j[1] is not math.inf:
+                if not visited[next_row(graph, j[1][0])] and j[1][1] < min and next_row(graph, j[1][0]) != index:
+                    curr = row
+                    next_city = next_row(graph, j[1][0])
+                    min = j[1][1]
             row += 1
         cities += 1  # We added one city.
         path.append(graph[index][curr])  # Appending the edge to the path.
@@ -170,12 +192,16 @@ def brute_force_tsp(graph):
 if __name__ == '__main__':
     doctest.testmod()
     print(construct_graph(tsp))
-    print(construct_graph(g, flag=False))
+    print(construct_graph(g))
     print(greedy_tsp(construct_graph(g)))
     print(greedy_tsp(construct_graph(tsp)))
-    print(TSP_PRINTER(algorithm=greedy_tsp, items=g))
     print(TSP_PRINTER(algorithm=greedy_tsp, items=g, outputtype=Output.sum))
     print(TSP_PRINTER(algorithm=greedy_tsp, items=tsp))
-    print(TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.sum, flag=True))
-    print(brute_force_tsp(construct_graph(tsp, flag=True)))
-    print(brute_force_tsp(construct_graph(g, flag=True)))
+    print(TSP_PRINTER(algorithm=brute_force_tsp, items=g, outputtype=Output.sum))
+    print(brute_force_tsp(construct_graph(tsp)))
+    print(brute_force_tsp(construct_graph(g)))
+    print(TSP_PRINTER(algorithm=greedy_tsp, items=tsp2, outputtype=Output.path))
+
+
+
+
